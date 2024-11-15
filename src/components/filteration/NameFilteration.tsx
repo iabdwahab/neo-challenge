0a671a5ed1e88d224833310d7c13a5bc978cbc58
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-11-15 06:45:15 +0200

    Making Filtering By Name Input.

--- src/components/filteration/NameFilteration.tsx
+++ src/components/filteration/NameFilteration.tsx
@@ -1,7 +1,27 @@
+import { ChangeEvent, useContext } from 'react';
+import { ApiDataContext } from '../../contexts/ApiDataContext';
+import { ViewedDataContext } from '../../contexts/ViewedDataContext';
+
 const NameFilteration = () => {
+  const apiData = useContext(ApiDataContext);
+  const { setViewedData } = useContext(ViewedDataContext);
+
+  // Filter Function
+  function filterByName(value: string) {
+    const filteredData = apiData.filter((object) => {
+      // Make User Value & ObjectName => Lowercase to match each other.
+      value = value.toLowerCase();
+      const objectName = object.name.toLowerCase();
+
+      return objectName.includes(value);
+    });
+
+    setViewedData(filteredData);
+  }
+
   return (
     <>
-      <input placeholder="Search By Name..." className="border rounded p-3" />
+      <input placeholder="Search By Name..." className="border rounded p-3" onChange={(e: ChangeEvent<HTMLInputElement>) => filterByName(e.target.value)} />
     </>
   );
 };
