4555bc9cd3d0acf1d7eaa25b22de3e32ce533021
Ibrahim AbdElwahab <iabdwahab20@gmail.com>
2024-11-15 17:02:22 +0200

    Implementing ChartView with ReactGoogleCharts.

--- src/components/chart_view/ChartView.tsx
+++ src/components/chart_view/ChartView.tsx
@@ -1,5 +1,32 @@
+import { useContext } from 'react';
+import { ViewedDataContext } from '../../contexts/ViewedDataContext';
+import Chart from 'react-google-charts';
+import { objectInterface } from '../../interfaces_types/interfaces_types';
+
 const ChartView = () => {
-  return <div>ChartView</div>;
+  const { viewedData } = useContext(ViewedDataContext);
+
+  const visualizedData: [string, number | string, number | string][] = [['Object', 'Min Diameter', 'Max Diameter']];
+
+  viewedData.forEach((object: objectInterface) => {
+    const minDiameter = object.estimated_diameter.kilometers.estimated_diameter_min;
+    const maxDiameter = object.estimated_diameter.kilometers.estimated_diameter_max;
+
+    visualizedData.push([object.name, minDiameter, maxDiameter]);
+  });
+
+  const options = {
+    title: 'NEO Diameter Visualization',
+    hAxis: {
+      title: 'Diameter (km)',
+      minValue: 0,
+    },
+    vAxis: {
+      title: 'NEO',
+    },
+  };
+
+  return <Chart chartType="BarChart" width="100%" height="400px" data={visualizedData} options={options} />;
 };
 
 export default ChartView;
